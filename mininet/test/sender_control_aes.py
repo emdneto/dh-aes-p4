853adc0f2c38ba480e7aadbf2fc54a0ce2aa3d3c
Emídio Neto <9735060+emdneto@users.noreply.github.com>
2021-05-14 14:16:31 -0300

    Update sender_control_aes.py

--- mininet/test/sender_control_aes.py
+++ mininet/test/sender_control_aes.py
@@ -1,4 +1,9 @@
+import sys
 import scapy.all as scapy
 
-input("Press Enter to continue...")
+if sys.version_info.major == 3:
+    input("Press Enter to continue...")
+else:
+    raw_input("Press Enter to continue...")
+
 scapy.srp1(scapy.Ether(type=0x812)/("\x00"*33), timeout=0)
